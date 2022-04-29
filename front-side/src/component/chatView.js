ccf8543330eadec31db10786133d04782bf5c07d
elproccess <aimensunabara@gmail.com>
2022-04-29 16:24:27 +0100

    socket connection

--- front-side/src/component/chatView.js
+++ front-side/src/component/chatView.js
@@ -1,7 +1,10 @@
+import { render } from '@testing-library/react';
 import React,{useState, useEffect}from 'react';
 
 import socket from '../global';
 import './chatView.css';
+
+
 
 function ChatView({chat}){
     const[data, setData] = useState([]);
@@ -9,22 +12,59 @@ function ChatView({chat}){
     const[cmessage, setcMessage] = useState([]);
     const[usersConntected, setUsersConntected] = useState([]);
 
-    useEffect(() => {
-        socket.on('history', (messages) => setData(messages) );
-       reciveingMessage();
-       }, []);
+   useEffect(() => {
 
+    socket.on('history', (messages) => setData(messages) );
+   reciveingMessage();
+   }, []);
 
-       return(
+   useEffect(() => {
+    setData((val) => [...val, cmessage]);
+   }, [cmessage]);
+
+   function reciveingUser(){
+    socket.on("sendUser", (user) => {
+        console.log(user);
+        setUsersConntected(user);
+    });
+   }
+
+   function reciveingMessage(){
+    socket.on('message',  (message) => {
+      setcMessage(message[0]);
+    });
+   }
+
+  function handleSubmit(e) {
+    e.preventDefault(); 
+      socket.emit("sendMessage", inputVal);
+    
+  }
+
+    return(
         <div className='chatview'>
             <div className='chat-screen'>
+                {data.map((val) => (
+                    <p>{val.quotes}</p>
+                ))}
+                
             </div>
 
         <form >
             <div className='form-container'>
                 <div className="input-container">
-              
+                    <input 
+                       
+                        onChange={(evt) => { setInputVal(evt.target.value) }}
+                        type="text"
+                        name='quotes' 
+                        placeholder="type your message...">
+                    </input>
                 
+                </div>
+                <div className='button-container'>
+                <button type='submit' onClick={(e) => handleSubmit(e)}>Submit</button>
+                   
                 </div>
             </div>
         </form>
@@ -32,5 +72,7 @@ function ChatView({chat}){
            
         </div>
     );
-
+    
 }
+
+export default ChatView;
---
+++ front-side/src/component/sideBar.js
@@ -0,0 +1,17 @@
+import React,{useState, useEffect}from 'react';
+
+import './sideBar.css';
+
+
+function SideBar(){
+    const[userCount, setUserCount] = useState(0);
+
+
+    return(
+        <div className='sidebar'>
+                <div className="img-container">
+                    </div>
+            </div>
+    );
+}
+export default SideBar;
